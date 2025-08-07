13e7a468ca6b35b3c308aaaf2a25b0a4efd78ee8
suma-soft <maciejsuchecki@hotmail.com>
2025-08-08 00:21:11 +0200

    debugging funkcjonowania BME oraz LVGL - dodany zegar CPU i bateria - działa

--- firmware/lvgl/heatbeat/main.c
+++ firmware/lvgl/heatbeat/main.c
@@ -3,49 +3,70 @@
 #include "pico/stdio_usb.h"
 #include "lvgl.h"
 #include "bme280_port.h"
+
 #include "../lv_port/lv_port_disp.h"
 #include "../lv_port/lv_port_indev.h"
+
 #include "bsp_i2c.h"
 #include "bsp_pcf85063.h"
+#include "bsp_battery.h"
+
+#include "hardware/adc.h"
+#include "hardware/clocks.h"
+#include "hardware/pll.h"
+#include "hardware/structs/pll.h"
+#include "hardware/structs/clocks.h"
 
 #define LVGL_TICK_MS 5
 
-static bool tick_cb(struct repeating_timer *t) { lv_tick_inc(LVGL_TICK_MS); return true; }
+// Funkcja ustawiająca zegar CPU – przeniesiona z poprzedniego UI
+void set_cpu_clock(uint32_t freq_Mhz)
+{
+    set_sys_clock_khz(freq_Mhz * 1000, true);
+    clock_configure(
+        clk_peri,
+        0,
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS,
+        freq_Mhz * 1000 * 1000,
+        freq_Mhz * 1000 * 1000
+    );
+}
+
+// Callback timera LVGL
+static bool tick_cb(struct repeating_timer *t) {
+    lv_tick_inc(LVGL_TICK_MS);
+    return true;
+}
 
 int main(void) {
     stdio_usb_init();
     printf("=== Program START (USB active) ===\n");
     printf("Startuję inicjalizację...\n");
 
-    // --- Inicjalizacja I2C1 ---
-    printf("Przed bsp_i2c_init (inicjalizacja I2C1 dla obu urządzeń)\n");
+    // --- Ustawienie zegara CPU na 250 MHz
+    set_cpu_clock(250);
+
+    // --- Inicjalizacja ADC i modułu baterii (z poprzedniego UI)
+    adc_init();
+    bsp_battery_init();
+    adc_set_temp_sensor_enabled(true);
+
+    // --- Inicjalizacja I2C1 (wspólne dla BME i RTC)
     bsp_i2c_init();
-    printf("Po bsp_i2c_init\n");
 
-    // --- Inicjalizacja RTC ---
-    printf("Przed bsp_pcf85063_init\n");
+    // --- Inicjalizacja RTC
     bsp_pcf85063_init();
-    printf("Po bsp_pcf85063_init\n");
 
-    // --- Inicjalizacja LVGL i wyświetlacza ---
-    printf("Przed lv_init\n");
+    // --- Inicjalizacja LVGL i wyświetlacza
     lv_init();
-    printf("Po lv_init\n");
-
-    printf("Przed lv_port_disp_init\n");
     lv_port_disp_init(466, 466, 0, false);
-    printf("Po lv_port_disp_init\n");
-
-    printf("Przed lv_port_indev_init\n");
     lv_port_indev_init(466, 466, 0);
-    printf("Po lv_port_indev_init\n");
 
-    // --- Inicjalizacja BME280 ---
-    printf("Przed bme280_init_default\n");
+    // --- Inicjalizacja BME280
     int8_t bme_status = bme280_init_default();
     printf("Po bme280_init_default, status: %d\n", bme_status);
 
-    // --- Ekran testowy LVGL ---
+    // --- Ekran testowy LVGL
     lv_obj_t *scr = lv_obj_create(NULL);
     lv_obj_t *lbl = lv_label_create(scr);
     lv_label_set_text(lbl, "LVGL+BME+RTC test");
@@ -57,7 +78,7 @@ int main(void) {
 
     lv_scr_load(scr);
 
-    // --- Timer LVGL ---
+    // --- Timer LVGL
     static struct repeating_timer t;
     add_repeating_timer_ms(LVGL_TICK_MS, tick_cb, NULL, &t);
 
@@ -78,7 +99,8 @@ int main(void) {
             // Odczytaj czas z RTC i pokaż na labelu
             bsp_pcf85063_get_time(&now_tm);
             char buf[32];
-            snprintf(buf, sizeof(buf), "%02d:%02d:%02d", now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec);
+            snprintf(buf, sizeof(buf), "%02d:%02d:%02d",
+                     now_tm.tm_hour, now_tm.tm_min, now_tm.tm_sec);
             lv_label_set_text(lbl_time, buf);
             last_time = now;
         }
@@ -88,7 +110,8 @@ int main(void) {
             printf("BME280 odczyt: %d\n", res);
             if (res == 0) {
                 char buf[64];
-                int n = snprintf(buf, sizeof(buf), "T:%.2fC H:%.1f%%", data.temperature, data.humidity);
+                snprintf(buf, sizeof(buf), "T:%.2fC H:%.1f%%",
+                         data.temperature, data.humidity);
                 lv_label_set_text(lbl, buf);
             } else {
                 lv_label_set_text(lbl, "BME error!");
