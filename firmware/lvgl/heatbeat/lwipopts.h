c1b85093704faafe1c5b57238bb386101e625162
suma-soft <maciejsuchecki@hotmail.com>
2025-10-28 14:20:39 +0100

    próba przejścia z RISC-V na ARM

--- firmware/lvgl/heatbeat/lwipopts.h
+++ firmware/lvgl/heatbeat/lwipopts.h
@@ -1,47 +1,26 @@
 #ifndef LWIPOPTS_H
 #define LWIPOPTS_H
 
-// NO_SYS (poll mode)
-#define NO_SYS                      1
-#define LWIP_NETCONN                0
+// Bazuj na domyślnych ustawieniach Pico SDK
+#include "lwipopts_examples_common.h"
+
+// SYS (wątki/RTOS mode)
+#define NO_SYS                      0
+#define LWIP_NETCONN                1
 #define LWIP_SOCKET                 0
 
+// Thread-safe
+#define LWIP_TCPIP_CORE_LOCKING     1
+#define LWIP_COMPAT_MUTEX_ALLOWED   1
+
 // Memory
-#define MEM_SIZE                    (8*1024)
+#define MEM_SIZE                    (16*1024)
 #define MEMP_NUM_TCP_PCB            8
-#define MEMP_NUM_TCP_PCB_LISTEN     4
-#define MEMP_NUM_TCP_SEG            16
-#define MEMP_NUM_PBUF               24
-#define PBUF_POOL_SIZE              24
+#define MEMP_NUM_NETCONN            8
 
 // TCP
-#define LWIP_TCP                    1
 #define TCP_MSS                     1460
-#define TCP_SND_BUF                 (4*TCP_MSS)
-#define TCP_WND                     (4*TCP_MSS)
-
-// DHCP
-#define LWIP_DHCP                   1
-#define DHCP_DOES_ARP_CHECK         0
-
-// DNS
-#define LWIP_DNS                    0
-
-// ICMP
-#define LWIP_ICMP                   1
-
-// Stats & Debug
-#define LWIP_STATS                  0
-#define LWIP_DEBUG                  0
-
-// Checksum offload
-#define CHECKSUM_GEN_IP             0
-#define CHECKSUM_GEN_UDP            0
-#define CHECKSUM_GEN_TCP            0
-#define CHECKSUM_GEN_ICMP           0
-#define CHECKSUM_CHECK_IP           0
-#define CHECKSUM_CHECK_UDP          0
-#define CHECKSUM_CHECK_TCP          0
-#define CHECKSUM_CHECK_ICMP         0
+#define TCP_SND_BUF                 (8*TCP_MSS)
+#define TCP_WND                     (8*TCP_MSS)
 
 #endif
